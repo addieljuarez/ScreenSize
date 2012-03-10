d6d51f88054fa6e53ebe9bfcd9accc190785f1a2
Addiel <root@addiel-VPCYB10AL.(none)>
2012-03-10 15:35:44 -0600

    view screen size

---
+++ Resources/app.js
@@ -0,0 +1,38 @@
+// 
+//  app.js
+//  screnSize
+//  
+//  Created by addiel on 2012-03-04.
+//  Copyright 2012 addiel. All rights reserved.
+// 
+
+//
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+//
+Titanium.UI.setBackgroundColor('#000');
+
+var win = Titanium.UI.createWindow({
+	backgroundColor:'#fff'
+});
+var ancho = Titanium.Platform.displayCaps.platformWidth;
+var alto = Titanium.Platform.displayCaps.platformHeight;
+
+var botonAncho = Titanium.UI.createButton({
+	title:'ancho',
+	top:'150dp'
+});
+win.add(botonAncho);
+botonAncho.addEventListener('click', function(e){
+	alert (ancho);
+});
+
+var botonAlto = Titanium.UI.createButton({
+	title:'alto',
+	top:'250'
+});
+win.add(botonAlto);
+botonAlto.addEventListener('click', function(e){
+	alert (alto);
+});
+
+win.open();
